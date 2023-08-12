bb930d07e2be8b4e0f8d269f72a1b072e0d7993e
NotNullBool <dptjvoy9@anonredirect.anonaddy.com>
2023-08-11 20:40:51 -0400

    day six part 2 done!

--- day_six/src/main.rs
+++ day_six/src/main.rs
@@ -3,13 +3,15 @@ use std::fs::read_to_string;
 fn main() {
     let input = read_to_string("./input.txt").unwrap();
 
-    let answer1 = first_marker_detect(input);
-    println!("{answer1:?} first answer!");
+    let answer1 = first_marker_detect(input.clone(), 4);
+    let answer2 = first_marker_detect(input, 14);
+
+    println!("{answer1:?} first answer!\n{answer2:?} second answer!");
 }
 
-fn first_marker_detect(input: String) -> usize {
+fn first_marker_detect(input: String, how_many_unqiue_chars: usize) -> usize {
     let temp = input.chars().collect::<Vec<_>>();
-    let four_at_a_time = temp.windows(4);
+    let four_at_a_time = temp.windows(how_many_unqiue_chars);
 
     'main: for (main_index, four_chars) in four_at_a_time.enumerate() {
         for (i, char1) in four_chars.iter().enumerate() {
@@ -22,7 +24,7 @@ fn first_marker_detect(input: String) -> usize {
                 }
             }
             if i == four_chars.len() - 1 {
-                return main_index + 4;
+                return main_index + how_many_unqiue_chars;
             }
         }
     }
